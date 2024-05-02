81dd8b1bbc2cb977cff59b4b64af8470ac656d76
AmosPeterAlex <amospeteralex28@gmail.com>
2024-05-02 11:29:08 +0530

    app config edited

--- lib/app_config/app_config.dart
+++ lib/app_config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
-  static String baseurl = "http://10.11.0.69:8000/accounts/";
-  static String mediaurl="http://10.11.0.69:8000/";
+  static String baseurl = "http://10.11.0.221:8000/accounts/";
+  static String mediaurl="http://10.11.0.221:8000/";
   static const String loginData = 'loginData';
   static const String status = "status";
   static const String token = "token";
